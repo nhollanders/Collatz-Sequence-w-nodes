4b8b8a93512653a4395e40b2815396f3b55dd71f
NonT <NonT5498@gmail.com>
2024-10-15 23:04:49 -0700

    fix bug with the linked list not counting correctly =+ instead of += oops

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <map>
 
 #include "includes/collatzFinder.h"
 
@@ -16,8 +15,40 @@ int main()
     {   
         cout << "Enter a integer less than or 100: ";
         
-        
+        string inputPar;
+        int number;
 
+        getline(cin, inputPar);
+
+        try
+        {
+            number = stoi(inputPar);
+            if ( number > 100 )
+            {
+                throw("Input exceeds number limit!");
+            }
+        }
+        catch(...)
+        {
+            cout << "Invalid input try again!" << endl;
+            continue;
+        }
+        
+        collatzFinder.findLongestSequence(number);
+        collatzFinder.displayLongestSequence();
+
+        cout << endl << "Would you like to enter another number (y/n)? ";
+        getline(cin, inputPar);
+        char anwser = inputPar[0];
+        if (anwser == 'y')
+        {
+            continue;
+        }
+        else
+        {
+            isRunning = false;
+            break;
+        }
     }
 
     return 0;
